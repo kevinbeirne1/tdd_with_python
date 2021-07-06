be2dc6eaa83825131b2bbe28210446cd35a7fa5d
kevinbeirne1 <kevingbeirne@gmail.com>
2021-07-06 16:53:35 -0400

    Moved HTML content from views to home.html template file

--- lists/tests.py
+++ lists/tests.py
@@ -15,8 +15,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do Lists</title>", html)
+        html = response.content.decode('utf8').strip()
+        self.assertTrue(html.startswith("<!DOCTYPE html>"))
+        self.assertIn("<title>To-Do lists</title>", html)
         self.assertTrue(html.endswith("</html>"))
 

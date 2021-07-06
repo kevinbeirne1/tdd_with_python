75b0ab5cba62cb58645eb39d1018736b83cdeb96
kevinbeirne1 <kevingbeirne@gmail.com>
2021-07-06 17:01:14 -0400

    Refactor tests.py to use self.client.get instead of HttpRequest

--- lists/tests.py
+++ lists/tests.py
@@ -8,15 +8,7 @@ from .views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page(self):
-        found = resolve("/")
-        self.assertEqual(found.func, views.home_page)
-
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8').strip()
-        self.assertTrue(html.startswith("<!DOCTYPE html>"))
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.endswith("</html>"))
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "lists/home.html")
 

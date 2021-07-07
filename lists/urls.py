54b2e80790685527141e389fad21aea0965fe5e6
kevinbeirne1 <kevingbeirne@gmail.com>
2021-07-07 17:49:41 -0400

    Moved app specific urls from superlists/urls to lists/urls

--- lists/urls.py
+++ lists/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
-url_patterns = [
-    url(r'^/$', views.home_page, name="home"),
+urlpatterns = [
+    url(r"new$", views.new_list, name="new_list"),
+    url(r"^(\d+)/$", views.view_list, name="view_list"),
+    url(r"^(\d+)/add_item", views.add_item, name="add_item"),
 ]

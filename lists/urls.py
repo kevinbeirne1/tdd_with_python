2b8c3873f4886d4712e7ce06fe5714f31ca865bb
kevinbeirne1 <kevingbeirne@gmail.com>
2021-07-08 12:28:49 -0400

    specified app_name, and changed form action from hard-coded url to django url tag

--- lists/urls.py
+++ lists/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = "lists"
+
 urlpatterns = [
     url(r"new$", views.new_list, name="new_list"),
     url(r"^(\d+)/$", views.view_list, name="view_list"),

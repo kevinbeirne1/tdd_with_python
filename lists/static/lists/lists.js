7e281162c452021bbca6188e263a6d4d5aaa2680
kevinbeirne1 <kevingbeirne@gmail.com>
2021-07-21 14:57:08 -0400

    added a listener to lists.js to remove error when clicking the input box

--- lists/static/lists/lists.js
+++ lists/static/lists/lists.js
@@ -1,7 +1,12 @@
 window.Superlists = {}
 window.Superlists.initialize = function () {
-    $('input[name="text"]').on('keypress', function () {
-        $('.has-error').hide()
-    });
+        $('input[name="text"]').on('keypress', () => {
+            $('.has-error').hide()
+        });
+
+        $('input[name="text"]').on('click', () => {
+            $('.has-error').hide()
+        });
+
 };
 

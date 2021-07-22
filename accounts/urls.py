48cfd9a9d1637edeb9aabe5f599a6e1d5e2ce996
kevinbeirne1 <kevingbeirne@gmail.com>
2021-07-22 11:14:36 -0400

    spiked in custom passwordless backend

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from accounts import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    url(r'^send_email$', views.send_login_email, name='send_login_email'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^logout$', views.logout, name='logout'),
+]
--- superlists/settings.py
+++ superlists/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-import dotenv
 import os
 
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "lists",
+    "accounts",
+]
+
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "kbeirneengineer@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True

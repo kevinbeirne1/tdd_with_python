5bd72e40d3ad26147740232fe78be30204707526
kevinbeirne1 <kevingbeirne@gmail.com>
2021-08-05 16:59:45 -0400

    Correcting typo in wait_for method

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -39,7 +39,7 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     @wait
     def wait_for(self, fn):
-        return fn
+        return fn ()
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')

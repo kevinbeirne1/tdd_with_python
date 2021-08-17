9d725be94a6c5ed115fb0007b7530e94c6ca6488
kevinbeirne1 <kevingbeirne@gmail.com>
2021-08-17 14:08:56 -0400

    Updated FT's to use management command for create pre-authenticated session, Updated reverted to separate host files for ansible playbook

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,32 +4,35 @@ import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from .server_tools import reset_database
 
 MAX_WAIT = 10
+
+
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
 
 
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get("STAGING_SERVER")
-        if staging_server:
-            self.live_server_url = "http://" + staging_server
+        self.staging_server = os.environ.get("STAGING_SERVER")
+        if self.staging_server:
+            self.live_server_url = "http://" + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self) -> None:
         self.browser.quit()
-
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
@@ -44,11 +47,24 @@ class FunctionalTest(StaticLiveServerTestCase):
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
 
+    # @wait
+    # def wait_to_be_logged_out(self, email):
+    #     self.browser.find_element_by_name('email')
+    #     navbar = self.browser.find_element_by_css_selector('.navbar')
+    #     self.assertNotIn(email, navbar.text)
+    #
+    # @wait
+    # def wait_to_be_logged_in(self, email):
+    #     self.browser.find_element_by_link_text('Log out')
+    #     navbar = self.browser.find_element_by_css_selector('.navbar')
+    #     self.assertIn(email, navbar.text)
+
     @wait
     def wait_to_be_logged_in(self, email):
         self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
+
 
     @wait
     def wait_to_be_logged_out(self, email):
